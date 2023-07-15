6bc6b9fe173cf836f10c27d559cf308ae0e82581
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2023-07-15 06:03:52 +0000

    int

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-import 
+from flask import Flask, request, jsonify
+from get import get_truecaller_info
+
+app = Flask(__name__)
+
+
+
+@app.route('/api/process/', methods=['GET'])
+def process():
+    phone_number = request.args.get('phonenumber')
+    
+
+    if phone_number:
+        result = get_truecaller_info(phone_number)
+        return jsonify(result)
+    else:
+        return jsonify({'error': 'Invalid phone number'}), 400
+
+if __name__ == '__main__':
+    app.run()
---
+++ get.py
@@ -0,0 +1,9 @@
+from truecallerpy import search_phonenumber
+import os
+
+def get_truecaller_info(phone_number):
+    api_key = os.environ.get('TRUECALLER_API_KEY')
+    country_code = "IN"
+    data = search_phonenumber(phone_number, country_code, api_key)
+    print(data)
+    return data
